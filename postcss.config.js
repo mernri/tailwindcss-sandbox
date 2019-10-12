1d49fba4b768d3a70df037e50479ac0ac0010323
Rita Mernissi <mernissi.rita@gmail.com>
2019-10-12 14:45:34 +0200

    added purgecss to remove unused CSS from production builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: [
+          // Add the path to all React components and pages
+          "./public/index.html"
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  ]
 };
-
